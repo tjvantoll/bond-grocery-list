cebd8ab075952e1adacdfa1e2be8f77b255f19ab
TJ VanToll <tj.vantoll@gmail.com>
2014-09-16 13:18:24 -0400

    Actual starting point

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 (function() {
-	var el = new Everlive("PLACE YOUR API KEY HERE");
+	var el = new Everlive("YOUR API KEY");
 
 	var groceryDataSource = new kendo.data.DataSource({
 		type: "everlive",
@@ -8,33 +8,9 @@
 		}
 	});
 
-	/*function closeDialog() {
-		$("#add").data("kendoMobileModalView").close();
-		$("#grocery").val("");
-	}*/
-
 	function initialize() {
 		var app = new kendo.mobile.Application(document.body, { skin: "flat" });
-		$("#grocery-list").kendoMobileListView({
-			dataSource: groceryDataSource,
-			template: "#: Name #"
-		});
 	}
-
-	/*window.addView = kendo.observable({
-		add: function() {
-			var grocery = $("#grocery").val();
-			if (grocery.trim() === "") {
-				navigator.notification.alert("Please provide a grocery.");
-				return;
-			}
-
-			groceryDataSource.add({ Name: grocery });
-			groceryDataSource.one("sync", closeDialog);
-			groceryDataSource.sync();
-		},
-		close: closeDialog
-	});*/
 
 	document.addEventListener("deviceready", initialize);
 }());
