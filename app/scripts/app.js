d2cda5c5daf91d99b78d7848d5e74f9ac45a1a45
TJ VanToll <tj.vantoll@gmail.com>
2014-09-18 17:29:07 -0400

    Avoid using the everlive shortcut and tabs --> spaces for consistency

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,28 @@
 (function() {
-	var el = new Everlive("YOUR API KEY");
+    var apiKey = "nMfdN0aMrPrHIqgv"
+    var el = new Everlive(apiKey);
 
-	var groceryDataSource = new kendo.data.DataSource({
-		type: "everlive",
-		transport: {
-			typeName: "Groceries"
-		}
-	});
+    var groceryDataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "http://api.everlive.com/v1/" + apiKey + "/Groceries",
+                dataType: "jsonp"
+            }
+        },
+        schema: {
+            data: function(response) {
+                return response.Result;
+            }
+        }
+    });
 
-	function initialize() {
-		var app = new kendo.mobile.Application(document.body, { skin: "flat" });
-	}
+    function initialize() {
+        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
+    }
 
-	document.addEventListener("deviceready", initialize);
+    document.addEventListener("deviceready", initialize);
 }());
