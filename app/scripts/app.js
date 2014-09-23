bc6f7c48c1e5884d4d4e264f73f25cb715039b8d
TJ VanToll <tj.vantoll@gmail.com>
2014-09-23 15:03:42 -0400

    Storing off state after lesson 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,41 @@
 (function() {
-    var apiKey = "YOUR API KEY"
+    var apiKey = "ZVJ8yTSrJPJmDYED"
     var el = new Everlive(apiKey);
 
     var groceryDataSource = new kendo.data.DataSource({
+        type: "everlive",
         transport: {
-            read: {
-                url: "https://api.everlive.com/v1/" + apiKey + "/Groceries",
-                dataType: "jsonp"
-            }
-        },
-        schema: {
-            data: function(response) {
-                return response.Result;
-            }
+            typeName: "Groceries"
         }
     });
 
     function initialize() {
         var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
     }
+
+    function closeDialog() {
+        $("#add").data("kendoMobileModalView").close();
+        $("#grocery").val("");
+    }
+
+    window.addView = kendo.observable({
+        add: function() {
+            var grocery = $("#grocery").val();
+            if (grocery.trim() === "") {
+                navigator.notification.alert("Please provide a grocery.");
+                return;
+            }
+
+            groceryDataSource.add({ Name: grocery });
+            groceryDataSource.one("sync", closeDialog);
+            groceryDataSource.sync();
+        },
+        close: closeDialog
+    });
 
     document.addEventListener("deviceready", initialize);
 }());
