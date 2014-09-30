a103cb9ebd716e198618d63f235d3d76d17a394f
TJ VanToll <tj.vantoll@gmail.com>
2014-09-30 16:09:08 -0400

    Saving off complete state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,8 @@
     var apiKey = "ZVJ8yTSrJPJmDYED"
     var el = new Everlive(apiKey);
 
+    window.Users = el.Users;
+
     var groceryDataSource = new kendo.data.DataSource({
         type: "everlive",
         transport: {
@@ -19,6 +21,83 @@
             template: "#: Name #"
         });
     }
+
+    window.loginView = kendo.observable({
+        submit: function() {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.login(this.username, this.password,
+                function(data) {
+                    window.location.href = "#list";
+                    groceryDataSource.read();
+                }, function() {
+                    navigator.notification.alert("Unfortunately we could not find your account.");
+                });
+        }
+    });
+
+    window.registerView = kendo.observable({
+        submit: function() {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, { Email: this.email },
+                function() {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function() {
+                    alert("Unfortunately we were unable to create your account.");
+                });
+        }
+    });
+
+    window.passwordView = kendo.observable({
+        submit: function() {
+            if (!this.email) {
+                navigator.notification.alert("Email address is required.");
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
+                contentType: "application/json",
+                data: JSON.stringify({ Email: this.email }),
+                success: function() {
+                    navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+                    window.location.href = "#login";
+                },
+                error: function() {
+                    navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
+                }
+            });
+        }
+    });
+
+    window.listView = kendo.observable({
+        logout: function(event) {
+            // Prevent going to the login page until the login call processes.
+            event.preventDefault();
+            el.Users.logout(function() {
+                this.loginView.set("username", "");
+                this.loginView.set("password", "");
+                window.location.href = "#login";
+            }, function() {
+                navigator.notification.alert("Unfortunately an error occurred logging out of your account.");
+            });
+        }
+    });
 
     window.addView = kendo.observable({
         add: function() {
